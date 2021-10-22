a5956fc191ce3e181a245ff7d740f80286134eaf
davifelinto <40410923+davifelinto@users.noreply.github.com>
2021-10-22 11:30:56 -0300

    Add files via upload

---
+++ selects_game_notes.sql
@@ -0,0 +1,27 @@
+use game_notes;
+
+-- Teste pra ver se a db ta funcionando
+select * from jogador;
+select * from raca;
+select * from classe;
+select * from item;
+select * from npc;
+select * from quest;
+select * from personagem;
+select * from itens_personagem;
+select * from quests_personagem;
+select * from lojas;
+select * from transacao;
+select * from itens_transacao;
+
+-- Mostrar quais jogadores estão ativos e seus respectivos personagens
+
+-- Mostrar personagens e os itens que ele possui?
+
+-- Mostrar o registro de vendas de uma loja
+
+-- Mostrar um personagem e as missões que ele está encarregado de fazer
+
+-- Mostrar uma operaçao de compra completa, com loja, personagem, jogador, item, preço...alter
+
+-- Pensar em outros selects
