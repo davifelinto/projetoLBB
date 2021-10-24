abe949af12c34e387f4c2e0a31ed0995d24aac27
Mateus Colares <mcolaresc@gmail.com>
2021-10-24 12:55:33 -0300

    adicionando alguns selects

--- selects_game_notes.sql
+++ selects_game_notes.sql
@@ -25,3 +25,36 @@ select * from itens_transacao;
 -- Mostrar uma operaçao de compra completa, com loja, personagem, jogador, item, preço...alter
 
 -- Pensar em outros selects
+
+-- select classe de cada personagem
+SELECT classe.nome, personagem.nome 
+FROM classe 
+JOIN personagem
+	ON personagem.id_classe = classe.id 
+ORDER BY classe.nome;
+
+-- select raça de cada personagem
+SELECT raca.nome, personagem.nome 
+FROM raca 
+JOIN personagem
+	ON personagem.id_raca = raca.id 
+ORDER BY raca.nome;
+
+-- numero total de personagens em cada raca
+SELECT raca.nome, COUNT(personagem.nome) AS quantidade
+FROM raca 
+JOIN personagem
+	ON personagem.id_raca = raca.id
+GROUP by raca.nome;
+
+-- numero total de personagens em cada classe
+SELECT classe.nome, COUNT(personagem.nome) AS quantidade
+FROM classe 
+JOIN personagem
+	ON personagem.id_classe = classe.id
+GROUP by classe.nome;
+
+-- jogadores com mais de um ano de cadastro
+SELECT nome, email
+FROM jogador
+WHERE id = ANY(SELECT id_jogador FROM personagem WHERE year(current_date()) - year(data_criacao) >= 1);
