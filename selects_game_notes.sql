ac072c3eefd331c181ec31725ef819a50cdb6002
davifelinto <40410923+davifelinto@users.noreply.github.com>
2021-10-26 11:37:24 -0300

    Ajustes de nomes nas tabelas
    Só alterei o nome das colunas em algumas tabelas, e juntei as 2 que mostram personagem_classe e personagem_raca em uma que mostra personagem_raça_classe.

--- selects_game_notes.sql
+++ selects_game_notes.sql
@@ -15,14 +15,14 @@ select * from transacao;
 select * from itens_transacao;
 
 -- Mostrar quais jogadores estão ativos e seus respectivos personagens
-SELECT jogador.nome, personagem.nome AS personagem
+SELECT jogador.nome AS jogador, personagem.nome AS personagem
 FROM personagem
 JOIN jogador
 	ON jogador.id = personagem.id_jogador
 WHERE jogador.ativo = true;
 
 -- Mostrar personagens e os itens que ele possui
-SELECT personagem.nome, item.nome, item.preco,  item.raridade
+SELECT personagem.nome AS personagem, item.nome AS item, item.preco,  item.raridade
 FROM personagem
 JOIN itens_personagem
 	ON personagem.id = itens_personagem.id_personagem
@@ -30,8 +30,8 @@ JOIN item
 	ON item.id = itens_personagem.id_item
 ORDER BY personagem.nome;
 
--- Mostrar o registro de vendas de uma loja
-SELECT lojas.nome, item.nome AS item, itens_transacao.preco_item, itens_transacao.quantidade
+-- Mostrar o registro de vendas das lojas
+SELECT lojas.nome AS loja, item.nome AS item, itens_transacao.preco_item AS preço , itens_transacao.quantidade, personagem.nome AS comprador
 FROM lojas
 JOIN transacao
 	ON transacao.id_loja = lojas.id
@@ -39,16 +39,19 @@ JOIN itens_transacao
 	ON itens_transacao.id_transacao = transacao.id
 JOIN item
 	ON item.id = itens_transacao.id_item
+JOIN personagem
+	ON transacao.id_personagem = personagem.id
 ORDER BY lojas.nome;
 
 -- Mostrar um personagem e as missões que ele está encarregado de fazer
-SELECT personagem.nome, quest.descricao AS quest
+SELECT personagem.nome AS personagem, quest.descricao AS quest
 FROM personagem
 JOIN quests_personagem
 	ON quests_personagem.id_personagem = personagem.id
 JOIN quest
 	ON quest.id = quests_personagem.id_quest
 ORDER BY personagem.nome;
+
 -- Mostrar uma operaçao de compra completa, com loja, personagem, jogador, item, preço...alter, ordenado por JOGADOR
 SELECT jogador.nome AS jogador, personagem.nome AS personagem, lojas.nome AS loja, transacao.dia_da_transacao, item.nome AS item, itens_transacao.quantidade, itens_transacao.valor_total
 FROM jogador
@@ -64,21 +67,14 @@ JOIN item
 	ON item.id = itens_transacao.id_item
 ORDER BY jogador.nome;
 
-
-
--- select classe de cada personagem
-SELECT classe.nome, personagem.nome 
-FROM classe 
-JOIN personagem
-	ON personagem.id_classe = classe.id 
-ORDER BY classe.nome;
-
--- select raça de cada personagem
-SELECT raca.nome, personagem.nome 
-FROM raca 
-JOIN personagem
-	ON personagem.id_raca = raca.id 
-ORDER BY raca.nome;
+-- select classe e raça de cada personagem
+SELECT personagem.nome AS prsonagem, raca.nome AS raca, classe.nome AS classe
+FROM personagem 
+JOIN classe
+	ON personagem.id_classe = classe.id
+JOIN raca
+	ON personagem.id_raca = raca.id
+ORDER BY personagem.nome;
 
 -- numero total de personagens em cada raca
 SELECT raca.nome, COUNT(personagem.nome) AS quantidade
@@ -88,26 +84,26 @@ JOIN personagem
 GROUP by raca.nome;
 
 -- numero total de personagens em cada classe
-SELECT classe.nome, COUNT(personagem.nome) AS quantidade
+SELECT classe.nome AS classe, COUNT(personagem.nome) AS quantidade
 FROM classe 
 JOIN personagem
 	ON personagem.id_classe = classe.id
 GROUP by classe.nome;
 
 -- jogadores com mais de um ano de cadastro
-SELECT nome, email
+SELECT nome AS jogador, email
 FROM jogador
 WHERE id = ANY(SELECT id_jogador FROM personagem WHERE year(current_date()) - year(data_criacao) >= 1);
 
 -- NPC e suas quests
-SELECT npc.nome, npc.localizacao, quest.descricao, quest.recompensa
+SELECT npc.nome AS npc, npc.localizacao, quest.descricao, quest.recompensa
 FROM npc 
 JOIN quest 
 	ON quest.id_npc = npc.id
 ORDER BY npc.nome;
 
 -- Personagens com quest ja concluidas e os detalhes de suas quests
-SELECT personagem.nome, npc.nome AS npc, quest.descricao, quest.recompensa
+SELECT personagem.nome AS personagem, npc.nome AS npc, quest.descricao AS missao_completa, quest.recompensa
 FROM personagem
 JOIN quests_personagem 
 	ON personagem.id = quests_personagem.id_personagem
@@ -122,7 +118,7 @@ SELECT AVG(preco) FROM item
 WHERE preco > 6; 
 
 -- Personagens com o preco total de seus itens maior que 100 ordenados de acordo com o preco total de seus itens
-SELECT personagem.nome, SUM(item.preco) * itens_personagem.quantidade AS precoTotalItens
+SELECT personagem.nome AS personagem, SUM(item.preco) * itens_personagem.quantidade AS precoTotalItens
 FROM personagem
 JOIN itens_personagem
 	ON itens_personagem.id_personagem = personagem.id
@@ -135,7 +131,7 @@ ORDER BY precoTotalItens;
 -- Personagens que foram cadastrados em 2021 com o preco total de seus itens e o email de seus jogadores,
 -- selecionando os que tiverem o preco total de seus itens maior que 600 moedas
 -- ordenados de acordo com o preco total de seus itens
-SELECT personagem.nome, jogador.email, SUM(item.preco) * itens_personagem.quantidade AS precoTotalItens
+SELECT personagem.nome AS personagem, jogador.email AS email_Jogador, SUM(item.preco) * itens_personagem.quantidade AS precoTotalItens
 FROM personagem
 JOIN jogador
 	ON jogador.id = personagem.id_jogador
